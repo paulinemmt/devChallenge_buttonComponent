a77c9e3bdd208408e55d1560b24e47cad46f7559
Pauline Momot <pauline.momot@gmail.com>
2021-01-14 14:36:46 +0100

    User story 1 & 2 complete

---
+++ index.js
@@ -0,0 +1,19 @@
+// Disable box-shadow
+
+$ (function () {
+
+    let $buttonForBoxShadow = $('.parent-box-shadow').next();
+    console.log($buttonForBoxShadow);
+
+    $buttonForBoxShadow.click(function(){
+        if(this.classList.contains('box-shadow')){
+            $(this).removeClass('box-shadow');
+            let $textToReplace = $(this).text().replace("Click to remove the shadow","Click to add a shadow");
+            $(this).text($textToReplace);
+    }else{
+        $(this).addClass('box-shadow');
+        let $textToReplace= $(this).text().replace("Click to add a shadow","Click to remove the shadow");
+        $(this).text($textToReplace);
+    }
+    })
+})
