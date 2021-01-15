51a03f2fb25e7f3749f8049c17e50033ba2030e5
Pauline Momot <pauline.momot@gmail.com>
2021-01-15 11:34:17 +0100

    User story 1, 2 fixed and user story 3 complete

--- index.js
+++ index.js
@@ -1,19 +1,78 @@
-// Disable box-shadow
+$(function () {
+  //Select button to change
+  let $myButton = $("#myButton");
 
-$ (function () {
+  // Type of button
+  buttonType = function () {
+    let $defaultButton = $("#default");
+    $defaultButton.click(function () {
+      $myButton.removeClass("outline");
+      $myButton.removeClass("text");
+    });
 
-    let $buttonForBoxShadow = $('.parent-box-shadow').next();
-    console.log($buttonForBoxShadow);
+    let $outlineButton = $("#outline");
+    $outlineButton.click(function () {
+      if (myButton.classList.contains("outline")) {
+        $myButton.removeClass("outline");
+      } else {
+        $myButton.addClass("outline");
+        $myButton.removeClass("text");
+      }
+    });
 
-    $buttonForBoxShadow.click(function(){
-        if(this.classList.contains('box-shadow')){
-            $(this).removeClass('box-shadow');
-            let $textToReplace = $(this).text().replace("Click to remove the shadow","Click to add the shadow");
-            $(this).text($textToReplace);
-    }else{
-        $(this).addClass('box-shadow');
-        let $textToReplace= $(this).text().replace("Click to add the shadow","Click to remove the shadow");
+    let $textButton = $("#text");
+    $textButton.click(function () {
+      if (myButton.classList.contains("text")) {
+        $myButton.removeClass("text");
+      } else {
+        $myButton.addClass("text");
+        $myButton.removeClass("outline");
+      }
+    });
+  };
+
+  // Disable box-shadow
+  boxShadow = function () {
+    let $buttonForBoxShadow = $(".parent-box-shadow").next();
+
+    $buttonForBoxShadow.click(function () {
+      if (this.classList.contains("box-shadow")) {
+        $(this).removeClass("box-shadow");
+        $myButton.removeClass("box-shadow");
+        let $textToReplace = $(this)
+          .text()
+          .replace("Click to remove the shadow", "Click to add the shadow");
         $(this).text($textToReplace);
-    }
-    })
-})
+      } else {
+        $(this).addClass("box-shadow");
+        $myButton.addClass("box-shadow");
+        let $textToReplace = $(this)
+          .text()
+          .replace("Click to add the shadow", "Click to remove the shadow");
+        $(this).text($textToReplace);
+      }
+    });
+  };
+
+  //Disabled button
+  disableButton = function () {
+    let $disabledButton = $("#disabled");
+
+    $disabledButton.click(function () {
+      if (this.classList.contains("disabled")) {
+        $myButton.removeAttr("disabled", "");
+        $myButton.addClass("default");
+        $(this).removeClass("disabled");
+      } else {
+        $myButton.attr("disabled", "");
+        $myButton.removeClass("default");
+        $(this).addClass("disabled");
+      }
+    });
+  };
+
+  //Function call
+  buttonType();
+  boxShadow();
+  disableButton();
+});
